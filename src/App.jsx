1d4a65a844a3d7585f3bb9a6ae1339f178f352b7
priyanshusinha11 <priyanshusinha.2807@gmail.com>
2024-05-24 17:01:06 +0530

    state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from "./appwrite/auth"
 import './App.css'
+import { login, logout } from './store/authSlice'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  console.log(import.meta.env.VITE_APP_APPWRITE_URL);
-  return (
-    <>
-      <h1>Blog app</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          <Outlet/>
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
+
 
 export default App
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header/Header"
+import Footer from "./Footer.jsx/Footer"
+
+export {
+    Header,
+    Footer
+}
