a19e6df94a3f8f6d0755770f0027292a08e744c1
priyanshusinha11 <priyanshusinha.2807@gmail.com>
2024-05-26 00:11:16 +0530

    components++

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import Header from "./Header/Header"
-import Footer from "./Footer.jsx/Footer"
+import Footer from "./Footer/Footer"
 import Container from "./container/Container"
 import Logo from "./Logo"
 import LogoutBtn from "./Header/LogoutBtn"
