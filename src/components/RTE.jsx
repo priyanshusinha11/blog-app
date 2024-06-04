a95bd34a77665f143be689e0d22fc6e16511dc41
priyanshusinha11 <priyanshusinha.2807@gmail.com>
2024-06-04 23:03:23 +0530

    tinymce api key leak fixed

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -13,7 +13,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
                 control={control}
                 render={({ field: { onChange } }) => (
                     <Editor
-                        apiKey='wxm7mhifl0yvu7fddqx0vtaa9tqsvb4fgzshnuao02823o15'
+                        apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
                         initialValue={defaultValue}
                         init={{
                             initialValue: defaultValue,
